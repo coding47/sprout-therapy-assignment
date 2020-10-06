03e19837afeb4e5da8428c47c8f3564dceb9e5c9
Valentyn Dorozhko <free@Valentyns-iMac-Pro.local>
2020-10-06 16:39:29 +0300

    Add components for handling and display

---
+++ src/components/DisplayResult.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+  return(
+    <section>
+      <h2>Result</h2>
+      <p>{props.Result}</p>
+    </section>
+  )
+}
---
+++ src/components/HandleExp.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import calcOutput from '../utils/calcOutput';
+import DisplayResult from './DisplayResult';
+
+export default () => {
+    const [A, setA ] = useState("false");
+    const [B, setB ] = useState("false");
+    const [C, setC ] = useState("false");
+    const [D, setD ] = useState(0);
+    const [E, setE ] = useState(0);
+    const [F, setF ] = useState(0);
+    const [result, setResult] = useState(0);
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      setResult(calcOutput(A, B, C, D, E, F));
+    }
+    
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+            <fieldset>
+                <label htmlFor="A">A</label>
+                <select id="A" value={A} onChange={(event) => setA(event.target.value)}>
+                    <option value="true">true</option>
+                    <option value="false">false</option>
+                </select>
+                <label htmlFor="B">B</label>
+                <select id="B" value={B} onChange={(event) => setB(event.target.value)}>
+                    <option value="true">true</option>
+                    <option value="false">false</option>
+                </select>
+                <label htmlFor="C">C</label>
+                <select id="C" value={C} onChange={(event) => setC(event.target.value)}>
+                    <option value="true">true</option>
+                    <option value="false">false</option>
+                </select>
+            </fieldset>
+            <fieldset>
+                <label htmlFor="D">D</label>
+                <input id="D" type="number" step="0.1" value={D} onChange={(event) => setD(event.target.value)}/>
+                <label htmlFor="E">E</label>
+                <input id="E" type="number" value={E} onChange={(event) => setE(event.target.value)} />
+                <label htmlFor="F">F</label>
+                <input id="F" type="number" value={F} onChange={(event) => setF(event.target.value)}/>
+            </fieldset>
+            <input type="submit"/>
+        </form>
+        <DisplayResult Result={result} />
+    </>
+    )
+}
